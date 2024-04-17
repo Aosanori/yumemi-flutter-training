ad79339090f0027793b79f9269275fb5f514d6bf
Norihide Oda <norihideoda@gmail.com>
2024-04-17 16:43:30 +0900

    ✨ 天気予報を表示できるように

--- lib/components/weather_information.dart
+++ lib/components/weather_information.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_training/weather/weather.dart';
+import 'package:flutter_training/weather/weather_page_view_model.dart';
 
 /// 中央の天気予報と気温を表示するコンポーネント
-class WeatherInformation extends StatelessWidget {
+class WeatherInformation extends ConsumerWidget {
   const WeatherInformation({super.key});
 
+  Widget weatherImg(Weather weather) {
+    switch (weather) {
+      case Weather.sunny:
+        return SvgPicture.asset('assets/sunny.svg');
+      case Weather.cloudy:
+        return SvgPicture.asset('assets/cloudy.svg');
+      case Weather.rainy:
+        return SvgPicture.asset('assets/rainy.svg');
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final labelLarge = Theme.of(context).textTheme.labelLarge!;
+    final weather = ref.watch(weatherPageViewModelProvider);
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        const AspectRatio(
+        AspectRatio(
           aspectRatio: 1,
-          child: Placeholder(),
+          child: weather != null ? weatherImg(weather) : const Placeholder(),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
