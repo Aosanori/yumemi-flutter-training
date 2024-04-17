770227abb19baf2b27bd24c6cafee070712c4c96
Norihide Oda <norihideoda@gmail.com>
2024-04-17 16:39:14 +0900

    ✨  WeatherRepositoryの作成

---
+++ lib/weather/weather_repository.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_training/weather/weather.dart';
+import 'package:flutter_training/weather/weather_data_source.dart';
+
+final weatherRepositoryProvider = Provider(
+  (ref) => WeatherRepository(
+    ref.read(weatherDataSourceProvider),
+  ),
+);
+
+class WeatherRepository {
+  WeatherRepository(this._weatherDataSource);
+  final WeatherDataSource _weatherDataSource;
+
+  Weather fetchWeather() {
+    //   try {
+    //     return Weather.values.byName(weatherString);
+    //   } on Exception catch (_, e) {}
+    final weatherString = _weatherDataSource.fetchWeather();
+    final weather = Weather.values.byName(weatherString);
+    return weather;
+  }
+}
