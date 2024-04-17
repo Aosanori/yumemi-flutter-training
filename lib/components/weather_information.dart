ee6f42514dc90874e3a130695b6dc774a2a8cc64
Norihide Oda <norihideoda@gmail.com>
2024-04-17 14:58:50 +0900

    ♻️ WeatherInformation内に含まれるContainerを除去

--- lib/components/weather_information.dart
+++ lib/components/weather_information.dart
@@ -2,42 +2,42 @@ import 'package:flutter/material.dart';
 
 class WeatherInformation extends StatelessWidget {
   const WeatherInformation({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const AspectRatio(
-            aspectRatio: 1,
-            child: Placeholder(),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  '** ℃',
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelLarge!
-                      .copyWith(color: Colors.blue),
-                ),
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const AspectRatio(
+          aspectRatio: 1,
+          child: Placeholder(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                '** ℃',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .copyWith(color: Colors.blue),
               ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  '** ℃',
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelLarge!
-                      .copyWith(color: Colors.red),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                '** ℃',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .copyWith(color: Colors.red),
               ),
-            ],
-          ),
-        ],
-      );
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
