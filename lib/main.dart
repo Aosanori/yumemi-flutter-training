301bf859b20b2a63058868a70e7cdd4d591d2526
Norihide Oda <norihideoda@gmail.com>
2024-04-17 13:55:07 +0900

    ✨ 天気予報表示部の作成

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,69 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  Widget weatherInformation(BuildContext context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const AspectRatio(
+            aspectRatio: 1,
+            child: Placeholder(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  'aaa',
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelLarge!
+                      .copyWith(color: Colors.blue),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  'bbb',
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelLarge!
+                      .copyWith(color: Colors.red),
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Flexible(
+              child: SizedBox(),
+            ),
+            Flexible(
+              flex: 2,
+              child: Column(
+                children: [
+                  const Expanded(
+                    child: SizedBox(),
+                  ),
+                  weatherInformation(context),
+                  Expanded(
+                    child: Container()
+                  ),
+                ],
+              ),
+            ),
+            const Flexible(
+              child: SizedBox(),
+            ),
+          ],
         ),
       ),
     );
