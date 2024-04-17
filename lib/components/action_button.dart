2ad21b5fe1d9ec8e3d5e9ee4252032624a7d2a61
Norihide Oda <norihideoda@gmail.com>
2024-04-17 14:59:25 +0900

    ♻️ ActionButton内に含まれるContainerを除去

--- lib/components/action_button.dart
+++ lib/components/action_button.dart
@@ -1,21 +1,31 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ActionButton extends StatelessWidget {
   const ActionButton({
-    super.key,
     required this.buttonTitle,
     required this.onPressed,
+    super.key,
   });
-  
+
   final String buttonTitle;
   final void Function()? onPressed;
 
   @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('buttonTitle', buttonTitle));
+    properties
+        .add(ObjectFlagProperty<void Function()?>.has('onPressed', onPressed));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 16),
-      child: TextButton(
-        onPressed: onPressed,
+    return TextButton(
+      onPressed: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
         child: Text(
           buttonTitle,
         ),
