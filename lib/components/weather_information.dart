0f41177217a70197b095ca1b5b8a2503680239c1
Norihide Oda <norihideoda@gmail.com>
2024-04-18 09:20:02 +0900

    ♻️ WeatherInformationのコメントを///に

--- lib/components/weather_information.dart
+++ lib/components/weather_information.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// 中央の天気予報と気温を表示するコンポーネント
+/// 中央の天気予報と気温を表示するコンポーネント
 class WeatherInformation extends StatelessWidget {
   const WeatherInformation({super.key});
 
