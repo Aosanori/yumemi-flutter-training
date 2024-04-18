5563c04cfab8aaaf35fd1abd395ed5a0239d5c1d
Norihide Oda <norihideoda@gmail.com>
2024-04-18 18:16:23 +0900

    ♻️ WeatherDataSourceProviderをriverpodGenelator仕様に書換

--- lib/weather/weather_data_source.dart
+++ lib/weather/weather_data_source.dart
@@ -1,10 +1,14 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_training/weather/yumemi_weather_service.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
-final weatherDataSourceProvider = Provider.autoDispose(
-  (ref) => WeatherDataSource(ref.watch(yumemiWeatherServiceProvider)),
-);
+part 'weather_data_source.g.dart';
+
+@riverpod
+WeatherDataSource weatherDataSource(WeatherDataSourceRef ref) {
+  final service = ref.watch(yumemiWeatherServiceProvider);
+  return WeatherDataSource(service);
+}
 
 class WeatherDataSource {
   WeatherDataSource(this._yumemiWeatherService);
---
+++ lib/weather/weather_data_source.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_data_source.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherDataSourceHash() => r'c17b987e747fbb19eaef0ea6382f624036f898f8';
+
+/// See also [weatherDataSource].
+@ProviderFor(weatherDataSource)
+final weatherDataSourceProvider =
+    AutoDisposeProvider<WeatherDataSource>.internal(
+  weatherDataSource,
+  name: r'weatherDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$weatherDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef WeatherDataSourceRef = AutoDisposeProviderRef<WeatherDataSource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
