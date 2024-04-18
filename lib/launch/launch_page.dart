64406ae762ba489466a0498c8677e73e092826b8
Norihide Oda <norihideoda@gmail.com>
2024-04-18 19:33:27 +0900

    ✨ LaunchPageの作成

---
+++ lib/launch/launch_page.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class LaunchPage extends StatefulWidget {
+  const LaunchPage({super.key});
+
+  @override
+  State<LaunchPage> createState() => LaunchPageState();
+}
+
+class LaunchPageState extends State<LaunchPage> {
+  @override
+  void initState() {
+    unawaited(
+      WidgetsBinding.instance.endOfFrame.then(_repeatPushingWithWaitingTime),
+    );
+    super.initState();
+  }
+
+  Future<void> _repeatPushingWithWaitingTime(void _) async {
+   await Future<void>.delayed(const Duration(milliseconds: 500));
+   await context.push('/weather');
+   await _repeatPushingWithWaitingTime(_);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.green),
+    );
+  }
+}
