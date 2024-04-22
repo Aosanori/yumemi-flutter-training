0df727278e4dbc8e0f72226102350c76440fe3f1
Norihide Oda <norihideoda@gmail.com>
2024-04-22 17:56:17 +0900

    💥 画面にWeatherDataの情報を表示できるように

---
+++ lib/components/temperature_display.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TemperatureDisplay extends StatelessWidget {
+  const TemperatureDisplay({
+    required int? temperature, required Color textColor, super.key,
+  })  : _textColor = textColor,
+        _temperature = temperature;
+  final int? _temperature;
+  final Color _textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle =
+        Theme.of(context).textTheme.labelLarge!.copyWith(color: _textColor);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Text(
+        _temperature != null ? '$_temperature ℃' : '** ℃',
+        style: textStyle,
+      ),
+    );
+  }
+}
