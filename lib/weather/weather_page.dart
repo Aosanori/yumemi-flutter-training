8efb2278637f052e636aa324e4c920422503e0e8
Norihide Oda <norihideoda@gmail.com>
2024-04-22 10:22:03 +0900

    ♻️ 不要なMaterialAppを削除

--- lib/weather/weather_page.dart
+++ lib/weather/weather_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_training/components/action_button.dart';
@@ -11,54 +10,40 @@ class WeatherPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final weatherNotifier = ref.read(weatherPageViewModelProvider.notifier);
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-        ),
-        brightness: Brightness.light,
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          brightness: Brightness.dark,
-        ),
-      ),
-      home: Scaffold(
-        // 真ん中で2:1の領域を作る
-        body: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.5,
-            child: Column(
-              children: [
-                const Spacer(),
-                const WeatherInformation(),
-                Expanded(
-                  child: Column(
-                    // SizedBoxを用いて
-                    // WeatherInformationとActionButtonの隙間を作る
-                    children: [
-                      const SizedBox(
-                        height: 80,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          ActionButton(
-                            buttonTitle: 'close',
-                            onPressed: () {},
-                          ),
-                          ActionButton(
-                            buttonTitle: 'reload',
-                            onPressed: weatherNotifier.fetchWeather,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+    return Scaffold(
+      // 真ん中で2:1の領域を作る
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: Column(
+            children: [
+              const Spacer(),
+              const WeatherInformation(),
+              Expanded(
+                child: Column(
+                  // SizedBoxを用いて
+                  // WeatherInformationとActionButtonの隙間を作る
+                  children: [
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ActionButton(
+                          buttonTitle: 'close',
+                          onPressed: () {},
+                        ),
+                        ActionButton(
+                          buttonTitle: 'reload',
+                          onPressed: weatherNotifier.fetchWeather,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
