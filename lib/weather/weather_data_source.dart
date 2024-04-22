506c1b6d97140e9caa75649254ca68b7c2c48327
Norihide Oda <norihideoda@gmail.com>
2024-04-22 17:52:53 +0900

    💥 DataSourceのfetchWeather()をAPIのfetchWeather()に接続

--- lib/weather/weather_data_source.dart
+++ lib/weather/weather_data_source.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter_training/exceptions/yumemi_weather_exception.dart';
 import 'package:flutter_training/weather/yumemi_weather_service.dart';
+import 'package:intl/intl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:simple_logger/simple_logger.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
@@ -16,11 +19,19 @@ class WeatherDataSource {
   WeatherDataSource(this._yumemiWeatherService);
   final YumemiWeather _yumemiWeatherService;
 
+  static final payload = {
+    'area': 'tokyo',
+    'date': DateFormat('yyyy-MM-dd hh:mm:ss').format(
+      DateTime(2023, 4, 1, 15, 30),
+    ),
+  };
+
   String fetchWeather() {
     try {
       SimpleLogger()
           .info('Getting weather data from Yumemi Weather Service...');
-      final weatherData = _yumemiWeatherService.fetchThrowsWeather('tokyo');
+      final weatherData =
+          _yumemiWeatherService.fetchWeather(json.encode(payload));
       SimpleLogger()
           .info('Complete to get weather data from Yumemi Weather Service!');
       return weatherData;
