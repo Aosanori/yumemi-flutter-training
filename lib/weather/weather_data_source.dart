bb36f41c3e7f0267f4a9d5505c0cca27d70bc3a8
Norihide Oda <norihideoda@gmail.com>
2024-04-23 16:50:12 +0900

    ✨ WeatherDataSourceにloggerを導入

--- lib/weather/weather_data_source.dart
+++ lib/weather/weather_data_source.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_training/exceptions/yumemi_weather_exception.dart';
 import 'package:flutter_training/weather/yumemi_weather_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:simple_logger/simple_logger.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 part 'weather_data_source.g.dart';
@@ -17,8 +18,14 @@ class WeatherDataSource {
 
   String fetchWeather() {
     try {
-      return _yumemiWeatherService.fetchThrowsWeather('tokyo');
+      SimpleLogger()
+          .info('Getting weather data from Yumemi Weather Service...');
+      final weatherData = _yumemiWeatherService.fetchThrowsWeather('tokyo');
+      SimpleLogger()
+          .info('Complete to get weather data from Yumemi Weather Service!');
+      return weatherData;
     } on YumemiWeatherError catch (e) {
+      SimpleLogger().shout('Failed to get data.');
       throw YumemiWeatherException(e);
     }
   }
