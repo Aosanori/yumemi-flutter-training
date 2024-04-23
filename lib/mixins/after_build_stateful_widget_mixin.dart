94753afec4afe8c68952ecbb3ad1ef4a8de3815a
Norihide Oda <norihideoda@gmail.com>
2024-04-23 10:03:31 +0900

    🐛 仕様の通りにレイアウトが表示された後に「何かしらの処理」を行う Mixin を作成

--- lib/mixins/after_build_stateful_widget_mixin.dart
+++ lib/mixins/after_build_stateful_widget_mixin.dart
@@ -1,20 +1,20 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 mixin AfterBuildStatefulWidgetMixin<T extends StatefulWidget> on State<T> {
-  Future<void> _redirectToWeatherPageWithWaitingTime(String url) async {
-    await Future<void>.delayed(const Duration(milliseconds: 500));
-    if (mounted) {
-      await context.push(url);
-    }
-    await _redirectToWeatherPageWithWaitingTime(url);
-  }
-
-  Future<void> redirectTo(String url) async {
-    await WidgetsBinding.instance.endOfFrame.then(
-      (_) {
-        _redirectToWeatherPageWithWaitingTime(url);
-      },
+  @override
+  void initState() {
+    super.initState();
+    unawaited(
+      WidgetsBinding.instance.endOfFrame.then(
+        (_) {
+          if (mounted) {
+            afterFirstBuild();
+          }
+        },
+      ),
     );
   }
+
+  void afterFirstBuild() {}
 }
